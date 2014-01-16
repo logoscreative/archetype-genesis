a6d68edd89817af8035ceaa95a92f524bd1908d9
Cliff Seal <cliff@logos-creative.com>
2014-01-16 07:37:26 -0500

    Adopt some initial Archetype features

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-=== Genesis Sample Theme ===
+=== Archetype for Genesis ===
 
-Github project link: https://github.com/copyblogger/genesis-sample
-
+This, my friends, is a work in progress.
 
 === Installation Instructions ===
 
-1. Upload the Genesis Sample theme folder via FTP to your wp-content/themes/ directory. (The Genesis parent theme needs to be in the wp-content/themes/ directory as well.)
+1. Upload the Archetype for Genesis theme folder via FTP to your wp-content/themes/ directory. (The Genesis parent theme needs to be in the wp-content/themes/ directory as well.)
 2. Go to your WordPress dashboard and select Appearance.
-3. Activate the Genesis Sample theme.
+3. Activate the Archetype for Genesis theme.
 4. Inside your WordPress dashboard, go to Genesis > Theme Settings and configure them to your liking.
-
-
-=== Theme Support ===
-
-Please visit http://my.studiopress.com/help/ for theme support.
--- functions.php
+++ functions.php
@@ -1,26 +1,77 @@
 <?php
-//* Start the engine
+/* ----------------------------------------------------------- *
+ * Start the engine
+ * ----------------------------------------------------------- */
 include_once( get_template_directory() . '/lib/init.php' );
 
-//* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.0.1' );
+/* ----------------------------------------------------------- *
+ * Child theme (do not remove)
+ * ----------------------------------------------------------- */
+define( 'CHILD_THEME_NAME', 'Archetype for Genesis' );
+define( 'CHILD_THEME_URL', 'http://logoscreative.co' );
+define( 'CHILD_THEME_VERSION', '1.0' );
 
-//* Enqueue Lato Google font
-add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
-function genesis_sample_google_fonts() {
-	wp_enqueue_style( 'google-font-lato', '//fonts.googleapis.com/css?family=Lato:300,700', array(), CHILD_THEME_VERSION );
+/* ----------------------------------------------------------- *
+ * Scripts
+ * ----------------------------------------------------------- */
+
+function archetype_enqueue_bootstrap() {
+
+	wp_register_style( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap-latest' );
+
+	wp_register_script( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js', array( 'jquery' ) );
+	wp_enqueue_script( 'bootstrap-latest' );
+
+	wp_register_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
+	wp_enqueue_style( 'font-awesome' );
+
 }
 
-//* Add HTML5 markup structure
+add_action( 'wp_enqueue_scripts', 'archetype_enqueue_bootstrap' );
+
+/* ----------------------------------------------------------- *
+ * Non-equeuable Scripts
+ * ----------------------------------------------------------- */
+
+function archetype_head_markup() {
+
+	// Touch Icon
+	$output = '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . apply_filters( 'archetype_touch_icon', get_stylesheet_directory_uri() . '/assets/touch-icon.png' ) . '" />';
+
+	// Help IE
+	$output .= '
+	<!--[if lt IE 9]>
+		<script type="text/javascript" src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
+		<link href="http://netdna.bootstrapcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+		<link href="' . get_template_directory_uri() . '/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
+		<script src="' . get_template_directory_uri() . '/cross-domain/respond.proxy.js"></script>
+        <![endif]-->';
+
+	echo $output;
+
+}
+
+add_action( 'wp_head', 'archetype_head_markup' );
+
+
+/* ----------------------------------------------------------- *
+ * Add HTML5 markup structure
+ * ----------------------------------------------------------- */
 add_theme_support( 'html5' );
 
-//* Add viewport meta tag for mobile browsers
+/* ----------------------------------------------------------- *
+ * Add viewport meta tag for mobile browsers
+ * ----------------------------------------------------------- */
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
+/* ----------------------------------------------------------- *
+ * Add support for custom background
+ * ----------------------------------------------------------- */
 add_theme_support( 'custom-background' );
 
-//* Add support for 3-column footer widgets
+/* ----------------------------------------------------------- *
+ * Add support for 3-column footer widgets
+ * ----------------------------------------------------------- */
 add_theme_support( 'genesis-footer-widgets', 3 );
