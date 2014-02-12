3a1e0a7ca20edcd4379748f4cdf8aba53e60365b
Cliff Seal <cliff@logos-creative.com>
2014-02-12 14:07:17 -0500

    Bump to Bootstrap 3.1.0

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ include_once( get_template_directory() . '/lib/init.php' );
  * ----------------------------------------------------------- */
 define( 'CHILD_THEME_NAME', 'Archetype for Genesis' );
 define( 'CHILD_THEME_URL', 'http://logoscreative.co' );
-define( 'CHILD_THEME_VERSION', '1.0.2' );
+define( 'CHILD_THEME_VERSION', '1.0.4' );
 
 /* ----------------------------------------------------------- *
  * Styles & Scripts
@@ -19,10 +19,10 @@ function archetype_enqueue_bootstrap() {
 
 	if ( !defined('ARCHETYPE_ENQUEUE') ) {
 
-		wp_register_style( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css' );
+		wp_register_style( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css' );
 		wp_enqueue_style( 'bootstrap-latest' );
 
-		wp_register_script( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js', array( 'jquery' ) );
+		wp_register_script( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js', array( 'jquery' ) );
 		wp_enqueue_script( 'bootstrap-latest' );
 
 		wp_register_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css' );
