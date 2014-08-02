92f9b607e13ce13d529879dd47dd0bc946818733
Cliff Seal <cliff@logos-creative.com>
2014-08-02 06:27:45 -0400

    Only run the proxy when using CDN

--- functions.php
+++ functions.php
@@ -51,15 +51,28 @@ function archetype_head_markup() {
 	// Touch Icon
 	$output = '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . apply_filters( 'archetype_touch_icon', get_stylesheet_directory_uri() . '/assets/dist/img/touch-icon.png' ) . '" />';
 
-	// Help IE
-	$output .= '
-	<!--[if lt IE 9]>
-		<script type="text/javascript" src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script>
-		<link href="http://netdna.bootstrapcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
-		<link href="' . get_template_directory_uri() . '/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
-		<script src="' . get_template_directory_uri() . '/cross-domain/respond.proxy.js"></script>
-        <![endif]-->';
+	if ( !defined('ARCHETYPE_ENQUEUE') ) {
+
+		// Help IE
+		$output .= '
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<link href="http://netdna.bootstrapcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+			<link href="' . get_template_directory_uri() . '/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
+			<script src="' . get_template_directory_uri() . '/cross-domain/respond.proxy.js"></script>
+			<![endif]-->';
+
+	} else {
+
+		// Help IE
+		$output .= '
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<![endif]-->';
+
+	}
 
 	echo $output;
 
