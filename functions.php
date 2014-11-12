11e1685762afdfa67f2ac8df9e5768011a572834
Cliff Seal <cliff@logos-creative.com>
2014-11-12 08:39:29 -0500

    Update Bootstrap and Font Awesome

--- functions.php
+++ functions.php
@@ -19,13 +19,13 @@ function archetype_enqueue_bootstrap() {
 
 	if ( !defined('ARCHETYPE_ENQUEUE') ) {
 
-		wp_register_style( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' );
+		wp_register_style( 'bootstrap-latest', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css' );
 		wp_enqueue_style( 'bootstrap-latest' );
 
-		wp_register_script( 'bootstrap-latest', '//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js', array( 'jquery' ) );
+		wp_register_script( 'bootstrap-latest', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js', array( 'jquery' ) );
 		wp_enqueue_script( 'bootstrap-latest' );
 
-		wp_register_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css' );
+		wp_register_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' );
 		wp_enqueue_style( 'font-awesome' );
 
 	} elseif ( defined('ARCHETYPE_ENQUEUE') && ARCHETYPE_ENQUEUE === 'bower' ) {
@@ -58,7 +58,7 @@ function archetype_head_markup() {
 		<!--[if lt IE 9]>
 			<script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			<link href="http://netdna.bootstrapcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+			<link href="http://maxcdn.bootstrapcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
 			<link href="' . get_template_directory_uri() . '/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
 			<script src="' . get_template_directory_uri() . '/cross-domain/respond.proxy.js"></script>
 			<![endif]-->';
